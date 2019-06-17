27edf28c01628abffebfef2580f966154095fdc2
iamatran <iamatran@hotmail.com>
2019-06-16 23:38:27 -0500

    updated logging

--- PythonLogstash/PythonLogstash/PythonLogstash.py
+++ PythonLogstash/PythonLogstash/PythonLogstash.py
@@ -12,6 +12,7 @@ game_logger.setLevel(logging.INFO)
 game_logger.addHandler(logstash.LogstashHandler('18.212.124.196', 5959, version=1))
 
 
+
 def randomPlay():
     return randint(0,2)
 
@@ -19,42 +20,55 @@ def randomPlay():
 def game():
     print "Welcome to my Rock Paper Scissors Game!"
     
-    for i in range(10):
+    for i in range(30):
         player1 = randomPlay()
         if player1 == 0:
-            game_logger.info("'player1Choice' : 'Rock'")
+            extra = {'player1Choice' : 'Rock'}
+            game_logger.info('outcome', extra=extra)
         elif player1 == 1:
-            game_logger.info("'player1Choice' : 'Paper'")
+            extra = {'player1Choice' : 'Paper'}
+            game_logger.info('outcome', extra=extra)
         elif player1 == 2:
-            game_logger.info("'player1Choice' : 'Scissors'")
+            extra = {'player1Choice' : 'Scissors'}
+            game_logger.info('outcome', extra=extra)
                 
         player2 = randomPlay()
         if player2 == 0:
-            game_logger.info("'player2Choice' : 'Rock'")
+            extra = {'player2Choice' : 'Rock'}
+            game_logger.info('outcome', extra=extra)
         elif player2 == 1:
-            game_logger.info("'player2Choice' : 'Paper'")
+            extra = {'player2Choice' : 'Paper'}
+            game_logger.info('outcome', extra=extra)
         elif player2 == 2:
-            game_logger.info("'player2Choice' : 'Scissors'")
+            extra = {'player2Choice' : 'Scissors'}
+            game_logger.info('outcome', extra=extra)
     
         
         if player1 == player2:
-            game_logger.warning("Detected collision of Player Choice")
-            game_logger.info("'win' : 'tie'")
+            game_logger.warning('Detected collision of Player Choice')
+            extra = {'win' : 'tie'}
+            game_logger.info('outcome', extra=extra)
                 
         if player1 == 0 and player2 == 1:
-            game_logger.info("'win' : 'player2'")
+            extra = {'win' : 'player2'}
+            game_logger.info('outcome', extra=extra)
         if player1 == 0 and player2 == 2:
-            game_logger.info("'win' : 'player1'")
+            extra = {'win' : 'player1'}
+            game_logger.info('outcome', extra=extra)
         if player1 == 1 and player2 == 0:
-            game_logger.info("'win' : 'player1'")
+            extra = {'win' : 'player1'}
+            game_logger.info('outcome', extra=extra)
         if player1 == 1 and player2 == 2:
-            game_logger.info("'win' : 'player2'")
+            extra = {'win' : 'player2'}
+            game_logger.info('outcome', extra=extra)
         if player1 == 2 and player2 == 0:
-            game_logger.info("'win' : 'player2'")
+            extra = {'win' : 'player2'}
+            game_logger.info('outcome', extra=extra)
         if player1 == 2 and player2 == 1:
-            game_logger.info("'win' : 'player1'")
+            extra = {'win' : 'player1'}
+            game_logger.info('outcome', extra=extra)
 
-
+   
 
 
 game()
